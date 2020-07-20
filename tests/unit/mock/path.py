9e7f4a58d3ef6d5ce188a18d4eb60f27c454fe29
NilashishC <nilashishchakraborty8@gmail.com>
2020-07-20 14:36:04 +0530

    Remove ignores
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- tests/unit/mock/path.py
+++ tests/unit/mock/path.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
 from ansible_collections.frr.frr.tests.unit.compat.mock import MagicMock
 from ansible.utils.path import unfrackpath
 
